741de92bda82cedf8bdb9ff22d51038a62d42eca
Ibrahim-Ali-942001 <ibrahim942001@gmail.com>
2022-07-27 22:27:21 +0500

    Database Created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserModule } from './user/user.module';
+import { User } from './user/entity/user.entity';
 
 @Module({
-  imports: [],
+  imports: [UserModule,TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'abc123',
+    database: 'blogbuzzdb',
+    entities: [User],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/user/entity/user.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+//   @OneToOne(()=> Blog)
+}
